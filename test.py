1ab63f39483c224e46ccb551db39625d63f7e22b
At11011 <nathaniel@swbell.net>
2022-10-08 09:50:01 -0500

    More test code

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Hello, world!")
+# import importlib
+# linalg = importlib.import_module('linalg', package='scipy')
+
+import scipy
+def main():
+  print("Hello, world!")
+  print("Scipy: {:.6f}".format(scipy.e))
+
+if __name__ == "__main__":
+  main()
